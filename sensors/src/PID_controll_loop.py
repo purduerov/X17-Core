68f73238f115327ffb8cced9f7f7cc718d9c0c03
Huajiqwq <daniel.solar17@outlook.com>
2023-11-04 16:08:43 -0400

    bug fixes

--- sensors/src/PID_controll_loop.py
+++ sensors/src/PID_controll_loop.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 
 next_state = 0.0
 depth = 0.0
-desire_state = 0.0
+desire_state = 0
 pid_enable = 0
 depth_plot = []
 thrust_plot = []
 time_counter = 1
+state =PID(1, 0.1, 0.05, setpoint=0)
 
 class PID_Depth_Controll_Pub(Node):
     def __init__(self):
@@ -23,39 +24,48 @@ class PID_Depth_Controll_Pub(Node):
         self.timer = self.create_timer(1, self.timer_callback)
 
     def timer_callback(self):
+        global next_state
+        global depth
+        global pid_enable
+        global state
+        global time_counter
+
+        # Assume we have a system we want to control in controlled_system
+        #v = controlled_system.update(0)
+
+        if pid_enable:
+            depth_plot.append(depth)
+            # Compute new output from the PID according to the systems current value
+            next_state = state.__call__(depth, time_counter)
+            thrust_plot.append(next_state)
+            
+            time_counter += 1
+            # Feed the PID output to the system and get its current value
+
+        if pid_enable: 
+            plt.plot(depth_plot, color = 'r', lable = 'depth')
+            plt.plot(thrust_plot, color = 'g', lable = 'thrust')
+            plt.legend()
+            plt.show
+        
         msg = Float64()
         msg.data = float(next_state)
         self.pub.publish(msg)
         
     def updatedepth(self, msg):
+        global depth
         depth = msg.data
         
     def updatedesirestate(self, msg):
+        global desire_state
+        global state
+        state.setpoint = desire_state
         desire_state = msg.data
         
     def undatepidtoggle(self, msg):
+        global pid_enable
         pid_enable = msg.data
         
-        
-state = PID(1, 0.1, 0.05, setpoint=desire_state)
-
-# Assume we have a system we want to control in controlled_system
-#v = controlled_system.update(0)
-
-while pid_enable:
-    depth_plot.append(depth)
-    # Compute new output from the PID according to the systems current value
-    next_state = state.__call__(depth, time_counter)
-    thrust_plot.append(next_state)
-    
-    time_counter += 1
-    # Feed the PID output to the system and get its current value
-
-if pid_enable: 
-    plt.plot(depth_plot, color = 'r', lable = 'depth')
-    plt.plot(thrust_plot, color = 'g', lable = 'thrust')
-    plt.legend()
-    plt.show
     
 def main(args=None):
     rclpy.init(args = args)
