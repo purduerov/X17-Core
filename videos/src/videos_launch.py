ef3370560ab5ab20682c802e9118d75308d36ba8
firestrom0986 <firestorm0986@gmail.com>
2025-09-27 14:54:57 -0400

    Changed camera port number

--- videos/src/videos_launch.py
+++ videos/src/videos_launch.py
@@ -8,7 +8,7 @@ import subprocess
 class Camera(Node):
     def __init__(self):
         super().__init__("cameras")
-        self.declare_parameter("dev_name", "device=/dev/video2")
+        self.declare_parameter("dev_name", "device=/dev/video19")
         self.declare_parameter("port_num", "5600")
 
         dev_name = self.get_parameter("dev_name").get_parameter_value().string_value
